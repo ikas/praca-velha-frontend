3f768b871c2f3a59ae052fd6f654cba3eaabe040
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-02 16:40:56 +0100

    feat: change home image

--- src/components/home-image/home-image-view.js
+++ src/components/home-image/home-image-view.js
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export default styled.div`
   width: 100%;
   height: 100vh;
-  background: url('/landing-bg.jpeg');
+  background: url('/landing-bg.jpg');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  background-position-y: top;
   display: flex;
   flex-direction: column;
   align-items: center;
