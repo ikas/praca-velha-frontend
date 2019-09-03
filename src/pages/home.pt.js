3cb5ab20b250a3577e7d3d32ef9c314b61279da0
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-03 17:23:01 +0100

    fix: remove unused fields

--- src/pages/home.pt.js
+++ src/pages/home.pt.js
@@ -50,7 +50,6 @@ export const pageQuery = graphql`
           city
           typologies
           status
-          saleStatus
           mainImage {
             fluid {
               ...GatsbyContentfulFluid
