305cf650cb0f0543e313b04ce3b137186c764f73
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-10 22:01:22 +0100

    feat: use image bg in landing pagee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from 'styled-components'
+import { Breakpoint, BreakpointProvider } from 'react-socks'
 
 import Heading from '../components/heading'
 import LanguageSelector from '../components/language-selector'
@@ -26,19 +27,40 @@ const VideoOverlay = styled.div`
   justify-content: center;
 `
 
+const LandingMobileImage = styled.div`
+  background: red;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  background: url('/landing-bg-mobile.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`
+
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <PageWrapper title="Choose your language">
-        <VideoBg autoPlay muted loop>
-          <source src="http://bragacenterapartments.com/wp-content/uploads/2019/01/braga.mp4" type="video/mp4" />
-        </VideoBg>
+      <BreakpointProvider>
+        <PageWrapper title="Choose your language">
+          <Breakpoint large up>
+            <VideoBg autoPlay muted loop>
+              <source src="http://bragacenterapartments.com/wp-content/uploads/2019/01/braga.mp4" type="video/mp4" />
+            </VideoBg>
+          </Breakpoint>
 
-        <VideoOverlay>
-          <Heading color="secondary" style={{ textAlign: 'center' }}>Welcome to Praça Velha</Heading>
-          <LanguageSelector />
-        </VideoOverlay>
-      </PageWrapper>
+          <Breakpoint medium down>
+            <LandingMobileImage />
+          </Breakpoint>
+
+          <VideoOverlay>
+            <Heading color="secondary" style={{ textAlign: 'center' }}>Welcome to Praça Velha</Heading>
+            <LanguageSelector />
+          </VideoOverlay>
+        </PageWrapper>
+      </BreakpointProvider>
     )
   }
 }
