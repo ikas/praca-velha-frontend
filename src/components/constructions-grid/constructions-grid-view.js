ba97201b124922c37e12c9156c320b7100397f08
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-11 16:02:04 +0100

    fix: problem with bg on construction wrapper

--- src/components/constructions-grid/constructions-grid-view.js
+++ src/components/constructions-grid/constructions-grid-view.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
-import { layout, space } from 'styled-system'
+import { color, layout, space } from 'styled-system'
 
 import Container from '../container'
 import ConstructionCard from '../construction-card'
 import Heading from '../heading'
 import ContentSeparator from '../content-separator'
+
+const BackgroundWrapper = styled.div`
+  ${color}
+`
 
 const StyledHeading = styled(Heading)`
   text-align: center;
@@ -23,14 +27,14 @@ const Constructions = styled.div`
 export default ({ title, constructions }) => {
   const { t } = useTranslation()
   return constructions.length > 0 && (
-    <>
+    <BackgroundWrapper bg="#eee">
       <ContentSeparator />
-      <Container bg="#eee">
+      <Container>
         <StyledHeading px={3} my={6} level={4}>{t(title)}</StyledHeading>
         <Constructions mb={7}>
           {constructions.map(({ node }) => <ConstructionCard {...node} key={node.id} />)}
         </Constructions>
       </Container>
-    </>
+    </BackgroundWrapper>
   )
 }
