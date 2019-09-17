fb7664bf12c8c99594bd837de7cec79c1da76ef8
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-17 15:38:11 +0100

    feat: add image gallery to construction page

---
+++ src/components/image-gallery/image-gallery-view.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
+export default props => <ImageGallery {...props} />
--- src/templates/construction-contentful.js
+++ src/templates/construction-contentful.js
@@ -1,44 +1,36 @@
 import React from 'react'
-import styled from 'styled-components'
 import { graphql } from 'gatsby'
-import { layout, space, color } from 'styled-system'
 
+import Box from '../components/box'
 import Container from '../components/container'
 import Heading from '../components/heading'
+import ImageGallery from '../components/image-gallery'
 import Layout from '../components/layout'
-import ConstructionCard from '../components/construction-card'
-
-const Background = styled.div`
-  ${color}
-`
-
-const ConstructionWrapper = styled.div`
-  ${layout}
-  ${space}
-  display: flex;
-  flex-wrap: wrap;
-`
-
-const Box = styled.div`
-  ${layout}
-  ${space}
-  ${color}
-`
 
 export default class ConstructionContentfulTemplate extends React.Component {
   render() {
     const construction = this.props.data.contentfulConstruction
+    const galleryItems = [
+      { original: construction.mainImage.fluid.src },
+      ...construction.images.map(img => ({ original: img.fluid.src })),
+    ];
     return (
       <Layout location={this.props.location} title={construction.name} headerAlwaysVisible={true}>
-        <Background bg="lightGrey">
-          <Container>
-            <ConstructionWrapper p={3} pt={7}>
-              <Box width={1/2} bg="white"><ConstructionCard {...construction} /></Box>
-              <Heading>{construction.name}</Heading>
+        <Container>
+          <Box display="flex" flexWrap="wrap" p={3} pt={7}>
+            <Box width={1/3}>
+              <Box mr={3}>
+                <ImageGallery disableThumbnailScroll={true} items={galleryItems} showPlayButton={false} />
+              </Box>
+            </Box>
+            <Box width={2/3}>
+              <Heading mt={0} mb={3}>{construction.name}</Heading>
+              <Heading my={3} level={4}>{construction.address} | {construction.city}</Heading>
+              <Heading mt={0} level={4}>{construction.typologies.join(' | ')}</Heading>
               <section dangerouslySetInnerHTML={{ __html: construction.description.childContentfulRichText.html }} />
-            </ConstructionWrapper>
-          </Container>
-        </Background>
+            </Box>
+          </Box>
+        </Container>
       </Layout>
     )
   }
@@ -58,9 +50,15 @@ export const pageQuery = graphql`
       address
       city
       country
+      typologies
       description {
         childContentfulRichText {
           html
+        }
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
         }
       }
       mainImage {
