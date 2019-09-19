a83ca8ed3c09cb70cfeecc7bb6787508d3242bd4
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-19 17:06:58 +0100

    fix: arrow navigation styles in image gallery

--- src/components/image-gallery/image-gallery-view.js
+++ src/components/image-gallery/image-gallery-view.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components'
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
-export default props => <ImageGallery {...props} />
+const NavButton = styled.button`
+  &::before {
+    text-shadow: 0 2px 2px ${props => props.theme.colors.primary} !important;
+    color: ${props => props.theme.colors.lightGrey} !important;
+  }
+
+  &:hover::before {
+    color: ${props => props.theme.colors.secondary} !important;
+  }
+`
+
+export default props => (
+  <ImageGallery
+    {...props}
+    renderLeftNav={(onClick, disabled) => {
+      return (
+        <NavButton
+          className='image-gallery-left-nav'
+          disabled={disabled}
+          onClick={onClick}/>
+      )
+    }}
+    renderRightNav={(onClick, disabled) => {
+      return (
+        <NavButton
+          className='image-gallery-right-nav'
+          disabled={disabled}
+          onClick={onClick}/>
+      )
+    }}
+  />
+)
