5cbe8d8ece9514b750c3a400b6d057afc6fb31fc
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-09-24 09:56:35 +0100

    fix: map settings

--- src/components/construction/map/map-container.js
+++ src/components/construction/map/map-container.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
+import Box from '../../box';
 import mapStyle from './map-style';
 
 export default class ConstructionMapContainer extends Component {
   render() {
     return (
-      <LoadScript googleMapsApiKey="AIzaSyClJe3o_eqRfd_5roc72kYNCR0CieFA_OQ">
-        <GoogleMap
-          options={{ styles: mapStyle }}
-          mapContainerStyle={{ height: "500px" }}
-          center={{ lat: this.props.lat, lng: this.props.lng }}
-          zoom={16}
-          clickableIcons={false}
-        >
-          <Marker position={{ lat: this.props.lat, lng: this.props.lng }} />
-        </GoogleMap>
-      </LoadScript>
+      <Box mb={7}>
+        <LoadScript googleMapsApiKey="AIzaSyClJe3o_eqRfd_5roc72kYNCR0CieFA_OQ">
+          <GoogleMap
+            options={{ styles: mapStyle }}
+            mapContainerStyle={{ height: "500px" }}
+            center={{ lat: this.props.lat, lng: this.props.lng }}
+            zoom={15}
+            clickableIcons={false}
+            >
+            <Marker position={{ lat: this.props.lat, lng: this.props.lng }} />
+          </GoogleMap>
+        </LoadScript>
+      </Box>
     );
   }
 }
