948b5c2e796e58e581028d27b0207953d4635922
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-10-02 17:31:05 +0100

    fix: construction map spaces

--- src/components/construction/map/map-container.js
+++ src/components/construction/map/map-container.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
-import Box from '../../box';
+import Container from '../../container';
 import mapStyle from './map-style';
 
 export default class ConstructionMapContainer extends Component {
   render() {
     return (
-      <Box mb={7}>
+      <Container pb={7}>
         <LoadScript googleMapsApiKey="AIzaSyClJe3o_eqRfd_5roc72kYNCR0CieFA_OQ">
           <GoogleMap
             options={{ styles: mapStyle }}
@@ -15,11 +15,11 @@ export default class ConstructionMapContainer extends Component {
             center={{ lat: this.props.lat, lng: this.props.lng }}
             zoom={15}
             clickableIcons={false}
-            >
+          >
             <Marker position={{ lat: this.props.lat, lng: this.props.lng }} />
           </GoogleMap>
         </LoadScript>
-      </Box>
+      </Container>
     );
   }
 }
