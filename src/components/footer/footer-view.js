1327da5cd2c0dfc87cf2bc6c4f56ce3fd0d7b6cd
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-10-08 14:43:41 +0100

    feat: improved footer styling

--- src/components/footer/footer-view.js
+++ src/components/footer/footer-view.js
@@ -1,16 +1,77 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMobile, faPhone, faAt } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
+import Box from '../box'
 import Copy from '../copy-text'
+import Container from '../container'
+import Heading from '../heading'
+import LogoSmall from '../logo-small'
+
+import theme from '../../utils/theme'
 
 const FooterWrapper = styled.div`
   background: ${props => props.theme.colors.primary};
 `
 
+const BorderedBox = styled(Box)`
+  border-bottom: 1px solid ${props => props.theme.colors.secondary};
+`
+
+const Anchor = styled.a`
+  color: ${props => props.theme.colors.white};
+`
+
 export default () => {
+  const year = (new Date()).getFullYear()
   return (
     <FooterWrapper>
-      <Copy color="white" py={3} style={{ textAlign: 'center' }}>© Praça Velha, 2019</Copy>
+      <Container>
+        <BorderedBox display="flex" mx={3} py={3} justifyContent="space-between">
+          <Box display="flex" alignItems="center" flex={1}>
+            <LogoSmall />
+            <Box ml={3}>
+              <Heading color="white" fontWeight="bold" level={5} mb={1}>
+                Praça Velha - Sociedade<br />Imobiliária S.A
+              </Heading>
+              <Copy color="white">Rua Justino da Cruz, Nº 154</Copy>
+              <Copy color="white">4700-314, Braga</Copy>
+            </Box>
+          </Box>
+
+          <Box flex={1} display="flex" alignItems="center" justifyContent="center">
+            <Box>
+              <Copy color="white" m={3} textAlign="center">
+                <FontAwesomeIcon icon={faMobile} size="lg" color={theme.colors.secondary} style={{ marginRight: '8px' }} />
+                +351 925 748 930
+              </Copy>
+              <Copy color="white" m={3} textAlign="center">
+                <FontAwesomeIcon icon={faPhone} size="lg" color={theme.colors.secondary} style={{ marginRight: '8px' }} />
+                +351 253 123 456
+              </Copy>
+            </Box>
+
+            <Box>
+              <Copy color="white" m={3} textAlign="center">
+                <FontAwesomeIcon icon={faAt} size="lg" color={theme.colors.secondary} style={{ marginRight: '8px' }} />
+                <Anchor href="mailto:info@pracavelha.pt">info@pracavelha.pt</Anchor>
+              </Copy>
+
+              <Box m={3}>
+                <a href="https://www.instagram.com/praca.velha">
+                  <FontAwesomeIcon icon={faFacebook} size="lg" color={theme.colors.secondary} style={{ marginRight: '8px' }} />
+                </a>
+                <a href="https://www.instagram.com/praca.velha">
+                  <FontAwesomeIcon icon={faInstagram} size="lg" color={theme.colors.secondary} style={{ marginRight: '8px' }} />
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        </BorderedBox>
+        <Copy color="white" py={3} textAlign="center">© Praça Velha, {year}</Copy>
+      </Container>
     </FooterWrapper>
   )
 }
---
+++ src/components/logo-small/logo-small-view.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from 'gatsby'
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "logo_vertical.png" }) {
+        childImageSharp {
+          fixed(width: 200) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fixed={data.file.childImageSharp.fixed} alt="Praça Velha logo" />
+}
