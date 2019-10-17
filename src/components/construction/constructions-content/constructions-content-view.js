e8f617cfc1c6de8cfc8d2b95d9f1d77da8bb505b
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-10-17 11:44:23 +0100

    feat: buildins page redesign

---
+++ src/components/construction/constructions-content/constructions-content-view.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { useTranslation } from 'react-i18next'
+
+import Box from '../../box'
+import ConstructionGrid from '../grid'
+import Heading from '../../heading'
+
+import PromoImage from '../promo-image'
+
+export default ({ constructions }) => {
+  const development = constructions.filter(({node}) => node.status === 'WIP')
+  const available = constructions.filter(({node}) => node.status === 'Ready')
+  const portfolio = constructions.filter(({node}) => node.status === 'Portfolio')
+  const { t } = useTranslation()
+  return (
+    <>
+      <PromoImage>
+        <Box bg="primarySoftShade" py={4} alignSelf="stretch" display="flex" justifyContent="center">
+          <Heading color="white">{t('Buildings')}</Heading>
+        </Box>
+      </PromoImage>
+      <ConstructionGrid title='Home Available Heading' constructions={available} bg="white" />
+      <ConstructionGrid title='Home Development Heading' constructions={development} bg="lightGrey" />
+      <ConstructionGrid title='Home Portfolio Heading' constructions={portfolio} bg="grey" includeContentSeparator={false} />
+    </>
+  )
+}
--- src/pages/empreendimentos.pt.js
+++ src/pages/empreendimentos.pt.js
@@ -1,32 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import AboutTextInBlack from '../components/about/text-in-black';
-import Box from '../components/box';
-import BuildingsPromoImage from '../components/construction/promo-image';
-import ConstructionsGrid from '../components/construction/grid'
-import Heading from '../components/heading';
+import ConstructionsContent from '../components/construction/constructions-content';
 import Layout from '../components/layout'
 
 export default class HomePage extends React.Component {
   render() {
-    const { edges } = this.props.data.allContentfulConstruction
-    const development = edges.filter(({ node }) => node.status === 'WIP')
-    const available = edges.filter(({ node }) => node.status === 'Ready')
-    const portfolio = edges.filter(({ node }) => node.status === 'Portfolio')
-    const { textInBlack } = this.props.data.allContentfulAbout.edges[0].node
     return (
       <Layout location={this.props.location} title="Empreendimentos">
-        <BuildingsPromoImage>
-          <Box bg="primarySoftShade" py={4} alignSelf="stretch" display="flex" justifyContent="center">
-            <Heading color="white">Empreendimentos</Heading>
-          </Box>
-        </BuildingsPromoImage>
-
-        <ConstructionsGrid title='Home Development Heading' constructions={development} />
-        <ConstructionsGrid title='Home Available Heading' constructions={available}  />
-        <ConstructionsGrid title='Home Portfolio Heading' constructions={portfolio} />
-        <AboutTextInBlack html={textInBlack.childContentfulRichText.html} />
+        <ConstructionsContent constructions={this.props.data.allContentfulConstruction.edges} />
       </Layout>
     )
   }
@@ -53,17 +35,6 @@ export const pageQuery = graphql`
           logoWhite {
             fixed(height: 100) {
               ...GatsbyContentfulFixed
-            }
-          }
-        }
-      }
-    }
-    allContentfulAbout(filter: { node_locale: { eq: "pt" }}) {
-      edges {
-        node {
-          textInBlack {
-            childContentfulRichText {
-              html
             }
           }
         }
