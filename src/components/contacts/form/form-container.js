13b33947f638e3a49a33ccb09524e9f12be475c3
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-10-22 16:51:23 +0100

    feat: form submission

--- src/components/contacts/form/form-container.js
+++ src/components/contacts/form/form-container.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { withTranslation } from 'react-i18next';
 
 import FormView from './form-view'
 
-export default class FormContainer extends React.Component {
+class FormContainer extends React.Component {
   constructor(props) {
     super(props)
 
@@ -22,13 +23,31 @@ export default class FormContainer extends React.Component {
     return this.setState(newState)
   }
 
-  onSubmit() {
-    console.log(this.state)
+  getSubject() {
+    const { t } = this.props
+    const { firstName, lastName, subject } = this.state
+    return encodeURI(`${t('Contacts Email Subject Start')} / ${firstName} ${lastName} / ${subject}`)
+  }
+
+  getBody() {
+    const { t } = this.props
+    const { phoneNumber, email, message } = this.state
+    return encodeURI(`${message}\n\n${t('Contacts Email')}: ${email}\n${t('Contacts Email Phone Number')}: ${phoneNumber}`)
+  }
+
+  onSubmit(ev) {
+    ev.preventDefault()
+
+    if( typeof document !== undefined) {
+      var mail = document.createElement("a");
+      mail.href = `mailto:info@pracavelha.pt?subject=${this.getSubject()}&body=${this.getBody()}`;
+      mail.click();
+    }
   }
 
   render() {
     return (
-      <form onSubmit={() => this.onSubmit()}>
+      <form onSubmit={(ev) => this.onSubmit(ev)}>
         <FormView
           handleChange={(f, v) => this.handleChange(f, v)}
           {...this.props}
@@ -38,3 +57,5 @@ export default class FormContainer extends React.Component {
     )
   }
 }
+
+export default withTranslation()(FormContainer)
