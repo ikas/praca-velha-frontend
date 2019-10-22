98f36bda4a4f67385822660839d73df1879c9174
Henrique Pacheco <henriquejcpacheco@gmail.com>
2019-10-22 16:56:44 +0100

    feat: contacts form theme customization

--- src/components/contacts/form/form-view.js
+++ src/components/contacts/form/form-view.js
@@ -2,6 +2,8 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 import styled from "styled-components"
 import TextField from '@material-ui/core/TextField'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { amber } from '@material-ui/core/colors'
 
 import Box from '../../box'
 import Container from '../../container'
@@ -16,6 +18,12 @@ const Button = styled.button`
   margin-top: ${props => props.theme.space[5]}px;
 `
 
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: amber,
+  },
+});
+
 export default ({
   firstName,
   lastName,
@@ -27,16 +35,60 @@ export default ({
 }) => {
   const { t } = useTranslation()
   return (
-    <Container>
-      <Box mt={6} mb={7} mx={3}>
-        <Heading level={4} textAlign="center">{t('Contacts Talk With Us')}</Heading>
+    <MuiThemeProvider theme={muiTheme}>
+      <Container>
+        <Box mt={6} mb={7} mx={3}>
+          <Heading level={4} textAlign="center">{t('Contacts Talk With Us')}</Heading>
 
-        <Box display="grid" gridTemplateColumns="50% 49%" gridGap={3}>
+          <Box display="grid" gridTemplateColumns="50% 49%" gridGap={3}>
+            <TextField
+              id="outlined-name"
+              label={t('Contacts Form Input First Name')}
+              value={firstName}
+              onChange={ev => handleChange('firstName', ev.target.value)}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+
+            <TextField
+              id="outlined-name"
+              label={t('Contacts Form Input Last Name')}
+              value={lastName}
+              onChange={ev => handleChange('lastName', ev.target.value)}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+
           <TextField
             id="outlined-name"
-            label={t('Contacts Form Input First Name')}
-            value={firstName}
-            onChange={ev => handleChange('firstName', ev.target.value)}
+            label={t('Contacts Form Input Email')}
+            value={email}
+            onChange={ev => handleChange('email', ev.target.value)}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+          />
+
+          <Box display="grid" gridTemplateColumns="50% 49%" gridGap={3}>
+            <TextField
+              id="outlined-name"
+              label={t('Contacts Form Input Phone Number')}
+              value={phoneNumber}
+              onChange={ev => handleChange('phoneNumber', ev.target.value)}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+
+          <TextField
+            id="outlined-name"
+            label={t('Contacts Form Input Subject')}
+            value={subject}
+            onChange={ev => handleChange('subject', ev.target.value)}
             margin="normal"
             variant="outlined"
             fullWidth
@@ -44,63 +96,21 @@ export default ({
 
           <TextField
             id="outlined-name"
-            label={t('Contacts Form Input Last Name')}
-            value={lastName}
-            onChange={ev => handleChange('lastName', ev.target.value)}
+            label={t('Contacts Form Input Message')}
+            value={message}
+            onChange={ev => handleChange('message', ev.target.value)}
             margin="normal"
             variant="outlined"
             fullWidth
+            multiline
+            rows="4"
           />
+
+          <Button>
+            <Copy color="secondary" fontWeight="bold" my={3}>{t('Contacts Form Submit')}</Copy>
+          </Button>
         </Box>
-
-        <TextField
-          id="outlined-name"
-          label={t('Contacts Form Input Email')}
-          value={email}
-          onChange={ev => handleChange('email', ev.target.value)}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-        />
-
-        <Box display="grid" gridTemplateColumns="50% 49%" gridGap={3}>
-          <TextField
-            id="outlined-name"
-            label={t('Contacts Form Input Phone Number')}
-            value={phoneNumber}
-            onChange={ev => handleChange('phoneNumber', ev.target.value)}
-            margin="normal"
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-
-        <TextField
-          id="outlined-name"
-          label={t('Contacts Form Input Subject')}
-          value={subject}
-          onChange={ev => handleChange('subject', ev.target.value)}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-        />
-
-        <TextField
-          id="outlined-name"
-          label={t('Contacts Form Input Message')}
-          value={message}
-          onChange={ev => handleChange('message', ev.target.value)}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          multiline
-          rows="4"
-        />
-
-        <Button>
-          <Copy color="secondary" fontWeight="bold" my={3}>{t('Contacts Form Submit')}</Copy>
-        </Button>
-      </Box>
-    </Container>
+      </Container>
+    </MuiThemeProvider>
   )
 }
